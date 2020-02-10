01c5ad13b7e02df87bc2d550a0c955cba0430b5b
Sudip Dutta <suddutt1@in.ibm.com>
2020-02-10 21:43:43 +0530

    Updated with new download path for fabric 1.4.2

--- generatescripts.go
+++ generatescripts.go
@@ -202,11 +202,10 @@ const _DOWNLOAD_SCRIPTS = `
 
 export VERSION={{.downloadVersion}}
 export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
-
 echo "===> Downloading platform binaries"
-export URL="https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${ARCH}-${VERSION}/hyperledger-fabric-${ARCH}-${VERSION}.tar.gz"
+export URL="https://github.com/hyperledger/fabric/releases/download/v${VERSION}/hyperledger-fabric-${ARCH}-${VERSION}.tar.gz"
 echo $URL
-curl  $URL| tar xz
+curl  -L $URL| tar xz
 
 `
 const _VERSION_COMP_MAP = `
